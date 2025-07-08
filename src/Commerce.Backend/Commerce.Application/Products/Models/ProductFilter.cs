32a448ff8c76c2047700c0f9ce04c059485c1936
Habiba Sattorova <habibasattorova26@gmail.com>
2025-07-09 03:22:06 +0500

    created product filter

---
+++ src/Commerce.Backend/Commerce.Application/Products/Models/ProductFilter.cs
@@ -0,0 +1,20 @@
+﻿using Commerce.Domain.Common.Queries;
+
+namespace Commerce.Application.Products.Models;
+
+public class ProductFilter : FilterPagination
+{
+    public override int GetHashCode()
+    {
+        var hashCode = new HashCode();
+
+        hashCode.Add(PageToken);
+        hashCode.Add(PageSize);
+
+        return hashCode.ToHashCode();
+    }
+
+    public override bool Equals(object? obj) =>
+        obj is ProductFilter filter &&
+        filter.GetHashCode() == GetHashCode();
+}
