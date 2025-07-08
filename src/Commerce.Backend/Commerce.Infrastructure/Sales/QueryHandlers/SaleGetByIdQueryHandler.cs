6f34970acac58559a0baaec07bd0b6b27101cb5b
Habiba Sattorova <habibasattorova26@gmail.com>
2025-07-09 03:39:00 +0500

    created sale queries

---
+++ src/Commerce.Backend/Commerce.Infrastructure/Sales/QueryHandlers/SaleGetByIdQueryHandler.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Commerce.Application.Sales.Models;
+using Commerce.Application.Sales.Queries;
+using Commerce.Application.Sales.Services;
+using Commerce.Domain.Common.Queries;
+
+namespace Commerce.Infrastructure.Sales.QueryHandlers;
+
+public class SaleGetByIdQueryHandler(
+    IMapper mapper,
+    ISaleService service)
+    : IQueryHandler<SaleGetByIdQuery, SaleGetDto>
+{
+    public async Task<SaleGetDto> Handle(SaleGetByIdQuery request, CancellationToken cancellationToken)
+    {
+        var result = await service.GetByIdAsync(request.SaleId, cancellationToken: cancellationToken);
+        return mapper.Map<SaleGetDto>(result);
+    }
+}
---
+++ src/Commerce.Backend/Commerce.Infrastructure/Sales/QueryHandlers/SaleGetQueryHandler.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using Commerce.Application.Sales.Models;
+using Commerce.Application.Sales.Queries;
+using Commerce.Application.Sales.Services;
+using Commerce.Domain.Common.Queries;
+using Commerce.Infrastructure.Common.Validators;
+using FluentValidation;
+using Microsoft.EntityFrameworkCore;
+
+namespace Commerce.Infrastructure.Sales.QueryHandlers;
+
+public class SaleGetQueryHandler(
+    IMapper mapper,
+    ISaleService service,
+    GetQueryValidator validationRules)
+    : IQueryHandler<SaleGetQuery, ICollection<SaleGetDto>>
+{
+    public async Task<ICollection<SaleGetDto>> Handle(SaleGetQuery request, CancellationToken cancellationToken)
+    {
+        var pagination = request.SaleFilter ?? new SaleFilter();
+        var validationResult = await validationRules.ValidateAsync(pagination);
+
+        if (!validationResult.IsValid)
+            throw new ValidationException(validationResult.Errors);
+
+        var result = await service.Get(
+            request.SaleFilter,
+            new QueryOptions { QueryTrackingMode = QueryTrackingMode.AsNoTracking })
+            .ToListAsync(cancellationToken);
+
+        return mapper.Map<ICollection<SaleGetDto>>(result);
+    }
+}
