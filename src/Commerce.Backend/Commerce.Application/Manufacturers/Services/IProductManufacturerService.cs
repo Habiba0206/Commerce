358349a059229c1df50e77aece5918d880530f5b
Habiba Sattorova <habibasattorova26@gmail.com>
2025-07-09 03:21:01 +0500

    created product manufacturer interface

---
+++ src/Commerce.Backend/Commerce.Application/Manufacturers/Services/IProductManufacturerService.cs
@@ -0,0 +1,56 @@
+﻿using Commerce.Application.Manufacturers.Models;
+using Commerce.Domain.Common.Commands;
+using Commerce.Domain.Common.Queries;
+using Commerce.Domain.Entities;
+using System.Linq.Expressions;
+
+namespace Commerce.Application.Manufacturers.Services;
+
+public interface IProductManufacturerService
+{
+    IQueryable<ProductManufacturer> Get(
+        Expression<Func<ProductManufacturer, bool>>? predicate = default, 
+        QueryOptions queryOptions = default);
+    
+    IQueryable<ProductManufacturer> Get(
+        ProductManufacturerFilter filter, 
+        QueryOptions queryOptions = default);
+    
+    ValueTask<ProductManufacturer?> GetByIdAsync(
+        Guid id, QueryOptions queryOptions = default, 
+        CancellationToken cancellationToken = default);
+    
+    ValueTask<bool> CheckByIdAsync(
+        Guid id, 
+        CancellationToken cancellationToken = default);
+    
+    ValueTask<IList<ProductManufacturer>> GetByIdsAsync(
+        IEnumerable<Guid> ids, 
+        QueryOptions queryOptions = default, 
+        CancellationToken cancellationToken = default);
+    
+    ValueTask<ProductManufacturer> CreateAsync(
+        ProductManufacturer item, 
+        CommandOptions commandOptions = default, 
+        CancellationToken cancellationToken = default);
+    
+    ValueTask<ProductManufacturer> UpdateAsync(
+        ProductManufacturer item, 
+        CommandOptions commandOptions = default, 
+        CancellationToken cancellationToken = default);
+    
+    ValueTask<ProductManufacturer> PatchAsync(
+        ProductManufacturerPatchDto patchDto, 
+        CommandOptions commandOptions = default, 
+        CancellationToken cancellationToken = default);
+    
+    ValueTask<ProductManufacturer?> DeleteAsync(
+        ProductManufacturer item, 
+        CommandOptions commandOptions = default, 
+        CancellationToken cancellationToken = default);
+    
+    ValueTask<ProductManufacturer?> DeleteByIdAsync(
+        Guid id, 
+        CommandOptions commandOptions = default, 
+        CancellationToken cancellationToken = default);
+}
