73ef835edfd265f316e17006162a498e42f3b07c
Habiba Sattorova <habibasattorova26@gmail.com>
2025-07-09 03:23:56 +0500

    created sale filter model

---
+++ src/Commerce.Backend/Commerce.Application/Sales/Models/SaleFilter.cs
@@ -0,0 +1,20 @@
+﻿using Commerce.Domain.Common.Queries;
+
+namespace Commerce.Application.Sales.Models;
+
+public class SaleFilter : FilterPagination
+{
+    public override int GetHashCode()
+    {
+        var hashCode = new HashCode();
+
+        hashCode.Add(PageToken);
+        hashCode.Add(PageSize);
+
+        return hashCode.ToHashCode();
+    }
+
+    public override bool Equals(object? obj) =>
+        obj is SaleFilter filter &&
+        filter.GetHashCode() == GetHashCode();
+}
