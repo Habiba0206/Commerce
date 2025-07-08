24ed0ddf58c7b125ccf94757636b2f3fbd9ae59d
Habiba Sattorova <habibasattorova26@gmail.com>
2025-07-09 03:25:11 +0500

    created section dto models

---
+++ src/Commerce.Backend/Commerce.Application/Sections/Models/SectionCreateUpdateDto.cs
@@ -0,0 +1,6 @@
+﻿namespace Commerce.Application.Sections.Models;
+
+public class SectionCreateUpdateDto
+{
+    public string Name { get; set; } // Unique
+}
---
+++ src/Commerce.Backend/Commerce.Application/Sections/Models/SectionPatchDto.cs
@@ -0,0 +1,7 @@
+﻿namespace Commerce.Application.Sections.Models;
+
+public class SectionPatchDto
+{
+    public Guid Id { get; set; }
+    public string? Name { get; set; } // Unique
+}
