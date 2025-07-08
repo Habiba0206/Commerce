f58b7ffbe0ec9d758ced1ab2c7718c8761cb0df5
Habiba Sattorova <habibasattorova26@gmail.com>
2025-07-09 03:39:55 +0500

    created section mapper

---
+++ src/Commerce.Backend/Commerce.Infrastructure/Sections/Mappers/SectionMapper.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Commerce.Application.Sections.Models;
+using Commerce.Domain.Entities;
+
+namespace Commerce.Infrastructure.Sections.Mappers;
+
+public class SectionMapper : Profile
+{
+    public SectionMapper()
+    {
+        CreateMap<Section, SectionCreateUpdateDto>().ReverseMap();
+        CreateMap<Section, SectionGetDto>().ReverseMap();
+        CreateMap<Section, SectionPatchDto>().ReverseMap();
+    }
+}
