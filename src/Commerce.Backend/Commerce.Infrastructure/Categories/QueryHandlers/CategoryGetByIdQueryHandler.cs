2c7a3c4bd7c8f88b9cff44b84744605cf922d243
Habiba Sattorova <habibasattorova26@gmail.com>
2025-07-09 03:31:40 +0500

    created category query handlers

---
+++ src/Commerce.Backend/Commerce.Infrastructure/Categories/QueryHandlers/CategoryGetByIdQueryHandler.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Commerce.Application.Categories.Models;
+using Commerce.Application.Categories.Queries;
+using Commerce.Application.Categories.Services;
+using Commerce.Domain.Common.Queries;
+
+namespace Commerce.Infrastructure.Categories.QueryHandlers;
+
+public class CategoryGetByIdQueryHandler(
+    IMapper mapper,
+    ICategoryService service)
+    : IQueryHandler<CategoryGetByIdQuery, CategoryGetDto>
+{
+    public async Task<CategoryGetDto> Handle(CategoryGetByIdQuery request, CancellationToken cancellationToken)
+    {
+        var result = await service.GetByIdAsync(request.CategoryId, cancellationToken: cancellationToken);
+        return mapper.Map<CategoryGetDto>(result);
+    }
+}
---
+++ src/Commerce.Backend/Commerce.Infrastructure/Categories/QueryHandlers/CategoryGetQueryHandler.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using Commerce.Application.Categories.Models;
+using Commerce.Application.Categories.Queries;
+using Commerce.Application.Categories.Services;
+using Commerce.Domain.Common.Queries;
+using Commerce.Infrastructure.Common.Validators;
+using FluentValidation;
+using Microsoft.EntityFrameworkCore;
+
+namespace Commerce.Infrastructure.Categories.QueryHandlers;
+
+public class CategoryGetQueryHandler(
+    IMapper mapper,
+    ICategoryService service,
+    GetQueryValidator validationRules)
+    : IQueryHandler<CategoryGetQuery, ICollection<CategoryGetDto>>
+{
+    public async Task<ICollection<CategoryGetDto>> Handle(CategoryGetQuery request, CancellationToken cancellationToken)
+    {
+        var pagination = request.CategoryFilter ?? new CategoryFilter();
+        var validationResult = await validationRules.ValidateAsync(pagination);
+
+        if (!validationResult.IsValid)
+            throw new ValidationException(validationResult.Errors);
+
+        var result = await service.Get(
+            request.CategoryFilter,
+            new QueryOptions { QueryTrackingMode = QueryTrackingMode.AsNoTracking })
+            .ToListAsync(cancellationToken);
+
+        return mapper.Map<ICollection<CategoryGetDto>>(result);
+    }
+}
