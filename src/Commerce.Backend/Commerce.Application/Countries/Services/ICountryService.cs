540b5d210229303ba4b33862f34a4abb747b1e63
Habiba Sattorova <habibasattorova26@gmail.com>
2025-07-09 03:19:09 +0500

    created country interface

---
+++ src/Commerce.Backend/Commerce.Application/Countries/Services/ICountryService.cs
@@ -0,0 +1,57 @@
+﻿using Commerce.Application.Countries.Models;
+using Commerce.Domain.Common.Commands;
+using Commerce.Domain.Common.Queries;
+using Commerce.Domain.Entities;
+using System.Linq.Expressions;
+
+namespace Commerce.Application.Countries.Services;
+
+public interface ICountryService
+{
+    IQueryable<Country> Get(
+        Expression<Func<Country, bool>>? predicate = default, 
+        QueryOptions queryOptions = default);
+    
+    IQueryable<Country> Get(
+        CountryFilter filter, 
+        QueryOptions queryOptions = default);
+    
+    ValueTask<Country?> GetByIdAsync(
+        Guid id, 
+        QueryOptions queryOptions = default, 
+        CancellationToken cancellationToken = default);
+    
+    ValueTask<bool> CheckByIdAsync(
+        Guid id, 
+        CancellationToken cancellationToken = default);
+    
+    ValueTask<IList<Country>> GetByIdsAsync(
+        IEnumerable<Guid> ids, 
+        QueryOptions queryOptions = default,
+        CancellationToken cancellationToken = default);
+    
+    ValueTask<Country> CreateAsync(
+        Country item, 
+        CommandOptions commandOptions = default, 
+        CancellationToken cancellationToken = default);
+    
+    ValueTask<Country> UpdateAsync(
+        Country item, 
+        CommandOptions commandOptions = default, 
+        CancellationToken cancellationToken = default);
+    
+    ValueTask<Country> PatchAsync(
+        CountryPatchDto patchDto, 
+        CommandOptions commandOptions = default, 
+        CancellationToken cancellationToken = default);
+    
+    ValueTask<Country?> DeleteAsync(
+        Country item, 
+        CommandOptions commandOptions = default, 
+        CancellationToken cancellationToken = default);
+    
+    ValueTask<Country?> DeleteByIdAsync(
+        Guid id, 
+        CommandOptions commandOptions = default, 
+        CancellationToken cancellationToken = default);
+}
