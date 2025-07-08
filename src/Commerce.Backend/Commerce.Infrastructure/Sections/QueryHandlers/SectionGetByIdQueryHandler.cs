477141f85f5007fad91555581e3ef992fd4d3355
Habiba Sattorova <habibasattorova26@gmail.com>
2025-07-09 03:40:06 +0500

    created section queries

---
+++ src/Commerce.Backend/Commerce.Infrastructure/Sections/QueryHandlers/SectionGetByIdQueryHandler.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Commerce.Application.Sections.Models;
+using Commerce.Application.Sections.Queries;
+using Commerce.Application.Sections.Services;
+using Commerce.Domain.Common.Queries;
+
+namespace Commerce.Infrastructure.Sections.QueryHandlers;
+
+public class SectionGetByIdQueryHandler(
+    IMapper mapper,
+    ISectionService service)
+    : IQueryHandler<SectionGetByIdQuery, SectionGetDto>
+{
+    public async Task<SectionGetDto> Handle(SectionGetByIdQuery request, CancellationToken cancellationToken)
+    {
+        var result = await service.GetByIdAsync(request.SectionId, cancellationToken: cancellationToken);
+        return mapper.Map<SectionGetDto>(result);
+    }
+}
---
+++ src/Commerce.Backend/Commerce.Infrastructure/Sections/QueryHandlers/SectionGetQueryHandler.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using Commerce.Application.Sections.Models;
+using Commerce.Application.Sections.Queries;
+using Commerce.Application.Sections.Services;
+using Commerce.Domain.Common.Queries;
+using Commerce.Infrastructure.Common.Validators;
+using FluentValidation;
+using Microsoft.EntityFrameworkCore;
+
+namespace Commerce.Infrastructure.Sections.QueryHandlers;
+
+public class SectionGetQueryHandler(
+    IMapper mapper,
+    ISectionService service,
+    GetQueryValidator validationRules)
+    : IQueryHandler<SectionGetQuery, ICollection<SectionGetDto>>
+{
+    public async Task<ICollection<SectionGetDto>> Handle(SectionGetQuery request, CancellationToken cancellationToken)
+    {
+        var pagination = request.SectionFilter ?? new SectionFilter();
+        var validationResult = await validationRules.ValidateAsync(pagination);
+
+        if (!validationResult.IsValid)
+            throw new ValidationException(validationResult.Errors);
+
+        var result = await service.Get(
+            request.SectionFilter,
+            new QueryOptions { QueryTrackingMode = QueryTrackingMode.AsNoTracking })
+            .ToListAsync(cancellationToken);
+
+        return mapper.Map<ICollection<SectionGetDto>>(result);
+    }
+}
