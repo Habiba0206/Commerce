37c8d2e637d34287010f85e9247535e6197f4bbe
Habiba Sattorova <habibasattorova26@gmail.com>
2025-07-09 03:22:19 +0500

    created product queries

---
+++ src/Commerce.Backend/Commerce.Application/Products/Queries/ProductGetByIdQuery.cs
@@ -0,0 +1,9 @@
+﻿using Commerce.Application.Products.Models;
+using Commerce.Domain.Common.Queries;
+
+namespace Commerce.Application.Products.Queries;
+
+public record ProductGetByIdQuery : IQuery<ProductGetDto?>
+{
+    public Guid ProductId { get; set; }
+}
