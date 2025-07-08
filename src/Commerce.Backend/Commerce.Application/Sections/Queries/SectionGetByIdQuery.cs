5b3ce9f734e16cc1da48b029c93acadd950dfe03
Habiba Sattorova <habibasattorova26@gmail.com>
2025-07-09 03:25:41 +0500

    created section queries

---
+++ src/Commerce.Backend/Commerce.Application/Sections/Queries/SectionGetByIdQuery.cs
@@ -0,0 +1,9 @@
+﻿using Commerce.Application.Sections.Models;
+using Commerce.Domain.Common.Queries;
+
+namespace Commerce.Application.Sections.Queries;
+
+public record SectionGetByIdQuery : IQuery<SectionGetDto?>
+{
+    public Guid SectionId { get; set; }
+}
---
+++ src/Commerce.Backend/Commerce.Application/Sections/Queries/SectionGetQuery.cs
@@ -0,0 +1,9 @@
+﻿using Commerce.Application.Sections.Models;
+using Commerce.Domain.Common.Queries;
+
+namespace Commerce.Application.Sections.Queries;
+
+public record SectionGetQuery : IQuery<ICollection<SectionGetDto>>
+{
+    public SectionFilter SectionFilter { get; set; }
+}
