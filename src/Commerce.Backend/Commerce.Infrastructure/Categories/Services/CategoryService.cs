a2fb77e65a42ebeb570f6b591b47ae9845bf8cdf
Habiba Sattorova <habibasattorova26@gmail.com>
2025-07-09 03:32:02 +0500

    implement category service

---
+++ src/Commerce.Backend/Commerce.Infrastructure/Categories/Services/CategoryService.cs
@@ -0,0 +1,86 @@
+﻿using Commerce.Application.Categories.Models;
+using Commerce.Application.Categories.Services;
+using Commerce.Domain.Common.Commands;
+using Commerce.Domain.Common.Exceptions;
+using Commerce.Domain.Common.Queries;
+using Commerce.Domain.Entities;
+using Commerce.Persistence.Extensions;
+using Commerce.Persistence.Repositories.Interfaces;
+using System.Linq.Expressions;
+
+namespace Commerce.Infrastructure.Categories.Services;
+
+public class CategoryService(
+    ICategoryRepository repository)
+    : ICategoryService
+{
+    public IQueryable<Category> Get(
+        Expression<Func<Category, bool>>? predicate = null, 
+        QueryOptions queryOptions = default) =>
+        repository.Get(predicate, queryOptions);
+
+    public IQueryable<Category> Get(
+        CategoryFilter filter, 
+        QueryOptions queryOptions = default) =>
+        repository.Get(queryOptions: queryOptions).ApplyPagination(filter);
+
+    public ValueTask<Category?> GetByIdAsync(
+        Guid id, 
+        QueryOptions queryOptions = default, 
+        CancellationToken cancellationToken = default) =>
+        repository.GetByIdAsync(id, queryOptions, cancellationToken);
+
+    public ValueTask<IList<Category>> GetByIdsAsync(
+        IEnumerable<Guid> ids, 
+        QueryOptions queryOptions = default, 
+        CancellationToken cancellationToken = default) =>
+        repository.GetByIdsAsync(ids, queryOptions, cancellationToken);
+
+    public ValueTask<bool> CheckByIdAsync(
+        Guid id, 
+        CancellationToken cancellationToken = default) =>
+        repository.CheckByIdAsync(id, cancellationToken);
+
+    public ValueTask<Category> CreateAsync(
+        Category entity, 
+        CommandOptions commandOptions = default, 
+        CancellationToken cancellationToken = default) =>
+        repository.CreateAsync(entity, commandOptions, cancellationToken);
+
+    public async ValueTask<Category> UpdateAsync(
+        Category entity, 
+        CommandOptions commandOptions = default, 
+        CancellationToken cancellationToken = default)
+    {
+        var existing = await repository.GetByIdAsync(entity.Id) ?? throw new NotFoundException(nameof(Category), entity.Id);
+        
+        existing.Name = entity.Name;
+        
+        return await repository.UpdateAsync(existing, commandOptions, cancellationToken);
+    }
+
+    public async ValueTask<Category> PatchAsync(
+        CategoryPatchDto patchDto, 
+        CommandOptions commandOptions = default, 
+        CancellationToken cancellationToken = default)
+    {
+        var existing = await repository.GetByIdAsync(patchDto.Id, cancellationToken: cancellationToken)
+                      ?? throw new NotFoundException(nameof(Category), patchDto.Id);
+        
+        if (patchDto.Name is not null) existing.Name = patchDto.Name;
+        
+        return await repository.UpdateAsync(existing, cancellationToken: cancellationToken);
+    }
+
+    public ValueTask<Category?> DeleteAsync(
+        Category entity, 
+        CommandOptions commandOptions = default, 
+        CancellationToken cancellationToken = default) =>
+        repository.DeleteAsync(entity, commandOptions, cancellationToken);
+
+    public ValueTask<Category?> DeleteByIdAsync(
+        Guid id, 
+        CommandOptions commandOptions = default, 
+        CancellationToken cancellationToken = default) =>
+        repository.DeleteByIdAsync(id, commandOptions, cancellationToken);
+}
