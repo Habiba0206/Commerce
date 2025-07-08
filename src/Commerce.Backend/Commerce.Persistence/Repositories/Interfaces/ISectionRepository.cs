b9ef1cdd189c0a9a8c883baeafcdbbfde31b47df
Habiba Sattorova <habibasattorova26@gmail.com>
2025-07-09 03:44:28 +0500

    created section repository

---
+++ src/Commerce.Backend/Commerce.Persistence/Repositories/Interfaces/ISectionRepository.cs
@@ -0,0 +1,47 @@
+﻿using Commerce.Domain.Common.Commands;
+using Commerce.Domain.Common.Queries;
+using Commerce.Domain.Entities;
+using System.Linq.Expressions;
+
+namespace Commerce.Persistence.Repositories.Interfaces;
+
+public interface ISectionRepository
+{
+    IQueryable<Section> Get(
+        Expression<Func<Section, bool>>? predicate = default,
+        QueryOptions queryOptions = default);
+
+    ValueTask<Section?> GetByIdAsync(
+        Guid id,
+        QueryOptions queryOptions = default,
+        CancellationToken cancellationToken = default);
+
+    ValueTask<bool> CheckByIdAsync(
+        Guid id,
+        CancellationToken cancellationToken = default);
+
+    ValueTask<IList<Section>> GetByIdsAsync(
+        IEnumerable<Guid> ids,
+        QueryOptions queryOptions = default,
+        CancellationToken cancellationToken = default);
+
+    ValueTask<Section> CreateAsync(
+        Section section,
+        CommandOptions commandOptions = default,
+        CancellationToken cancellationToken = default);
+
+    ValueTask<Section> UpdateAsync(
+        Section section,
+        CommandOptions commandOptions = default,
+        CancellationToken cancellationToken = default);
+
+    ValueTask<Section?> DeleteAsync(
+        Section section,
+        CommandOptions commandOptions = default,
+        CancellationToken cancellationToken = default);
+
+    ValueTask<Section?> DeleteByIdAsync(
+        Guid id,
+        CommandOptions commandOptions = default,
+        CancellationToken cancellationToken = default);
+}
