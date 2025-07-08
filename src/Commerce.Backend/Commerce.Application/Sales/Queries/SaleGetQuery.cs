3f2ea85287631bafebdb3b6d60e6207b09cfa32d
Habiba Sattorova <habibasattorova26@gmail.com>
2025-07-09 03:24:22 +0500

    created sale queries

---
+++ src/Commerce.Backend/Commerce.Application/Sales/Queries/SaleGetQuery.cs
@@ -0,0 +1,9 @@
+﻿using Commerce.Application.Sales.Models;
+using Commerce.Domain.Common.Queries;
+
+namespace Commerce.Application.Sales.Queries;
+
+public record SaleGetQuery : IQuery<ICollection<SaleGetDto>>
+{
+    public SaleFilter SaleFilter { get; set; }
+}
