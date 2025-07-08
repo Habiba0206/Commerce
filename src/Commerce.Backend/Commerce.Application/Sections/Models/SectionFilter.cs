6578395d036c7933c6c9c26b2173987abdeaacc7
Habiba Sattorova <habibasattorova26@gmail.com>
2025-07-09 03:25:22 +0500

    created section queries

---
+++ src/Commerce.Backend/Commerce.Application/Sections/Models/SectionFilter.cs
@@ -0,0 +1,20 @@
+﻿using Commerce.Domain.Common.Queries;
+
+namespace Commerce.Application.Sections.Models;
+
+public class SectionFilter : FilterPagination
+{
+    public override int GetHashCode()
+    {
+        var hashCode = new HashCode();
+
+        hashCode.Add(PageToken);
+        hashCode.Add(PageSize);
+
+        return hashCode.ToHashCode();
+    }
+
+    public override bool Equals(object? obj) =>
+        obj is SectionFilter filter &&
+        filter.GetHashCode() == GetHashCode();
+}
