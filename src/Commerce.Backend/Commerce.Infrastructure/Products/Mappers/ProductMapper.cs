909ec32c7bfbcdac6d45e06f98d403a324d0495d
Habiba Sattorova <habibasattorova26@gmail.com>
2025-07-09 03:37:39 +0500

    created product mapper

---
+++ src/Commerce.Backend/Commerce.Infrastructure/Products/Mappers/ProductMapper.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Commerce.Application.Products.Models;
+using Commerce.Domain.Entities;
+
+namespace Commerce.Infrastructure.Products.Mappers;
+
+public class ProductMapper : Profile
+{
+    public ProductMapper()
+    {
+        CreateMap<Product, ProductCreateUpdateDto>().ReverseMap();
+        CreateMap<Product, ProductGetDto>().ReverseMap();
+        CreateMap<Product, ProductPatchDto>().ReverseMap();
+    }
+}
