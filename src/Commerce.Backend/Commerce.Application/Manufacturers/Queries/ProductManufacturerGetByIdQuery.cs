b76d7346655a2e2f00db0f882bcd6663e2e10d1d
Habiba Sattorova <habibasattorova26@gmail.com>
2025-07-09 03:20:47 +0500

    created product manufacturer queries

---
+++ src/Commerce.Backend/Commerce.Application/Manufacturers/Queries/ProductManufacturerGetByIdQuery.cs
@@ -0,0 +1,9 @@
+﻿using Commerce.Application.Manufacturers.Models;
+using Commerce.Domain.Common.Queries;
+
+namespace Commerce.Application.Manufacturers.Queries;
+
+public record ProductManufacturerGetByIdQuery : IQuery<ProductManufacturerGetDto?>
+{
+    public Guid ProductManufacturerId { get; set; }
+}
---
+++ src/Commerce.Backend/Commerce.Application/Manufacturers/Queries/ProductManufacturerGetQuery.cs
@@ -0,0 +1,9 @@
+﻿using Commerce.Application.Manufacturers.Models;
+using Commerce.Domain.Common.Queries;
+
+namespace Commerce.Application.Manufacturers.Queries;
+
+public record ProductManufacturerGetQuery : IQuery<ICollection<ProductManufacturerGetDto>>
+{
+    public ProductManufacturerFilter ProductManufacturerFilter { get; set; }
+}
