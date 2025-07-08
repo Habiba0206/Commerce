448f2814ad06e53bc6b545fce2f06d5da5c70e01
Habiba Sattorova <habibasattorova26@gmail.com>
2025-07-09 03:36:08 +0500

    created product manufacturer mapper

---
+++ src/Commerce.Backend/Commerce.Infrastructure/Manufacturers/Mappers/ProductManufacturerMapper.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Commerce.Application.Manufacturers.Models;
+using Commerce.Domain.Entities;
+
+namespace Commerce.Infrastructure.Manufacturers.Mappers;
+
+public class ProductManufacturerMapper : Profile
+{
+    public ProductManufacturerMapper()
+    {
+        CreateMap<ProductManufacturer, ProductManufacturerCreateUpdateDto>().ReverseMap();
+        CreateMap<ProductManufacturer, ProductManufacturerGetDto>().ReverseMap();
+        CreateMap<ProductManufacturer, ProductManufacturerPatchDto>().ReverseMap();
+    }
+}
