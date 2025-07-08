c65028b88170bc849c95533e482bb6f0a2529459
Habiba Sattorova <habibasattorova26@gmail.com>
2025-07-09 03:15:07 +0500

    created category pagination model

---
+++ src/Commerce.Backend/Commerce.Application/Categories/Models/CategoryFilter.cs
@@ -0,0 +1,20 @@
+﻿using Commerce.Domain.Common.Queries;
+
+namespace Commerce.Application.Categories.Models;
+
+public class CategoryFilter : FilterPagination
+{
+    public override int GetHashCode()
+    {
+        var hashCode = new HashCode();
+
+        hashCode.Add(PageToken);
+        hashCode.Add(PageSize);
+
+        return hashCode.ToHashCode();
+    }
+
+    public override bool Equals(object? obj) =>
+        obj is CategoryFilter filter &&
+        filter.GetHashCode() == GetHashCode();
+}
