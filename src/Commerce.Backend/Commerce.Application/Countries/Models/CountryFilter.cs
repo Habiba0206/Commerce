63c0c4fc449b55f43f38f8da7fb797b6cac73df2
Habiba Sattorova <habibasattorova26@gmail.com>
2025-07-09 03:18:17 +0500

    created country filter

---
+++ src/Commerce.Backend/Commerce.Application/Countries/Models/CountryFilter.cs
@@ -0,0 +1,20 @@
+﻿using Commerce.Domain.Common.Queries;
+
+namespace Commerce.Application.Countries.Models;
+
+public class CountryFilter : FilterPagination
+{
+    public override int GetHashCode()
+    {
+        var hashCode = new HashCode();
+
+        hashCode.Add(PageToken);
+        hashCode.Add(PageSize);
+
+        return hashCode.ToHashCode();
+    }
+
+    public override bool Equals(object? obj) =>
+        obj is CountryFilter filter &&
+        filter.GetHashCode() == GetHashCode();
+}
