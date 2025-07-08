61b94b4ea40adb897e2ce3c04c7ba69123b81355
Habiba Sattorova <habibasattorova26@gmail.com>
2025-07-09 03:36:23 +0500

    created product manufacturer query handlers

---
+++ src/Commerce.Backend/Commerce.Infrastructure/Manufacturers/Queries/ProductManufacturerGetByIdQueryHandler.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Commerce.Application.Manufacturers.Models;
+using Commerce.Application.Manufacturers.Queries;
+using Commerce.Application.Manufacturers.Services;
+using Commerce.Domain.Common.Queries;
+
+namespace Commerce.Infrastructure.Manufacturers.Queries;
+
+public class ProductManufacturerGetByIdQueryHandler(
+    IMapper mapper,
+    IProductManufacturerService service)
+    : IQueryHandler<ProductManufacturerGetByIdQuery, ProductManufacturerGetDto>
+{
+    public async Task<ProductManufacturerGetDto> Handle(ProductManufacturerGetByIdQuery request, CancellationToken cancellationToken)
+    {
+        var result = await service.GetByIdAsync(request.ProductManufacturerId, cancellationToken: cancellationToken);
+        return mapper.Map<ProductManufacturerGetDto>(result);
+    }
+}
---
+++ src/Commerce.Backend/Commerce.Infrastructure/Manufacturers/Queries/ProductManufacturerGetQueryHandler.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using Commerce.Application.Manufacturers.Models;
+using Commerce.Application.Manufacturers.Queries;
+using Commerce.Application.Manufacturers.Services;
+using Commerce.Domain.Common.Queries;
+using Commerce.Infrastructure.Common.Validators;
+using FluentValidation;
+using Microsoft.EntityFrameworkCore;
+
+namespace Commerce.Infrastructure.Manufacturers.Queries;
+
+public class ProductManufacturerGetQueryHandler(
+    IMapper mapper,
+    IProductManufacturerService service,
+    GetQueryValidator validationRules)
+    : IQueryHandler<ProductManufacturerGetQuery, ICollection<ProductManufacturerGetDto>>
+{
+    public async Task<ICollection<ProductManufacturerGetDto>> Handle(ProductManufacturerGetQuery request, CancellationToken cancellationToken)
+    {
+        var pagination = request.ProductManufacturerFilter ?? new ProductManufacturerFilter();
+        var validationResult = await validationRules.ValidateAsync(pagination);
+
+        if (!validationResult.IsValid)
+            throw new ValidationException(validationResult.Errors);
+
+        var result = await service.Get(
+            request.ProductManufacturerFilter,
+            new QueryOptions { QueryTrackingMode = QueryTrackingMode.AsNoTracking })
+            .ToListAsync(cancellationToken);
+
+        return mapper.Map<ICollection<ProductManufacturerGetDto>>(result);
+    }
+}
