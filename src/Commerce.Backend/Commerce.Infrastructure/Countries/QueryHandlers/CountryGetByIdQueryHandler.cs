3630692f0f6d56f5f4eb8b1f261702e4efd0328b
Habiba Sattorova <habibasattorova26@gmail.com>
2025-07-09 03:34:52 +0500

    created country queries

---
+++ src/Commerce.Backend/Commerce.Infrastructure/Countries/QueryHandlers/CountryGetByIdQueryHandler.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Commerce.Application.Countries.Models;
+using Commerce.Application.Countries.Queries;
+using Commerce.Application.Countries.Services;
+using Commerce.Domain.Common.Queries;
+
+namespace Commerce.Infrastructure.Countries.QueryHandlers;
+
+public class CountryGetByIdQueryHandler(
+    IMapper mapper,
+    ICountryService service)
+    : IQueryHandler<CountryGetByIdQuery, CountryGetDto>
+{
+    public async Task<CountryGetDto> Handle(CountryGetByIdQuery request, CancellationToken cancellationToken)
+    {
+        var result = await service.GetByIdAsync(request.CountryId, cancellationToken: cancellationToken);
+        return mapper.Map<CountryGetDto>(result);
+    }
+}
---
+++ src/Commerce.Backend/Commerce.Infrastructure/Countries/QueryHandlers/CountryGetQueryHandler.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using Commerce.Application.Countries.Models;
+using Commerce.Application.Countries.Queries;
+using Commerce.Application.Countries.Services;
+using Commerce.Domain.Common.Queries;
+using Commerce.Infrastructure.Common.Validators;
+using FluentValidation;
+using Microsoft.EntityFrameworkCore;
+
+namespace Commerce.Infrastructure.Countries.QueryHandlers;
+
+public class CountryGetQueryHandler(
+    IMapper mapper,
+    ICountryService service,
+    GetQueryValidator validationRules)
+    : IQueryHandler<CountryGetQuery, ICollection<CountryGetDto>>
+{
+    public async Task<ICollection<CountryGetDto>> Handle(CountryGetQuery request, CancellationToken cancellationToken)
+    {
+        var pagination = request.CountryFilter ?? new CountryFilter();
+        var validationResult = await validationRules.ValidateAsync(pagination);
+
+        if (!validationResult.IsValid)
+            throw new ValidationException(validationResult.Errors);
+
+        var result = await service.Get(
+            request.CountryFilter,
+            new QueryOptions { QueryTrackingMode = QueryTrackingMode.AsNoTracking })
+            .ToListAsync(cancellationToken);
+
+        return mapper.Map<ICollection<CountryGetDto>>(result);
+    }
+}
