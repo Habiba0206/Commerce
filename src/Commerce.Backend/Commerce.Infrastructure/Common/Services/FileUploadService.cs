f7dfd446b9b49e9a1c99e16fc8f873da3e2896de
Habiba Sattorova <habibasattorova26@gmail.com>
2025-07-09 03:33:36 +0500

    implemented file upload service

---
+++ src/Commerce.Backend/Commerce.Infrastructure/Common/Services/FileUploadService.cs
@@ -0,0 +1,34 @@
+﻿using Commerce.Application.Common.Services;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+
+namespace Commerce.Infrastructure.Common.Services;
+
+public class FileUploadService : IFileUploadService
+{
+    private readonly IWebHostEnvironment _env;
+    private readonly IHttpContextAccessor _http;
+
+    public FileUploadService(IWebHostEnvironment env, IHttpContextAccessor http)
+    {
+        _env = env;
+        _http = http;
+    }
+
+    public async ValueTask<string> UploadImageAsync(IFormFile file)
+    {
+        var uploadsPath = Path.Combine(_env.WebRootPath ?? "wwwroot", "uploads", "blocks");
+        Directory.CreateDirectory(uploadsPath);
+
+        var fileExt = Path.GetExtension(file.FileName);
+        var fileName = $"{Guid.NewGuid()}{fileExt}";
+        var filePath = Path.Combine(uploadsPath, fileName);
+
+        using var stream = new FileStream(filePath, FileMode.Create);
+        await file.CopyToAsync(stream);
+
+        var request = _http.HttpContext?.Request;
+        return $"{request?.Scheme}://{request?.Host}/uploads/products/{fileName}";
+    }
+
+}
