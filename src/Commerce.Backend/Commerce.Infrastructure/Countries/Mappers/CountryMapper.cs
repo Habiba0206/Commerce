2e6b54c90804d53f1a87cab5b41f864c14374569
Habiba Sattorova <habibasattorova26@gmail.com>
2025-07-09 03:34:13 +0500

    created country mapper

---
+++ src/Commerce.Backend/Commerce.Infrastructure/Countries/Mappers/CountryMapper.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Commerce.Application.Countries.Models;
+using Commerce.Domain.Entities;
+
+namespace Commerce.Infrastructure.Countries.Mappers;
+
+public class CountryMapper : Profile
+{
+    public CountryMapper()
+    {
+        CreateMap<Country, CountryCreateUpdateDto>().ReverseMap();
+        CreateMap<Country, CountryGetDto>().ReverseMap();
+        CreateMap<Country, CountryPatchDto>().ReverseMap();
+    }
+}
