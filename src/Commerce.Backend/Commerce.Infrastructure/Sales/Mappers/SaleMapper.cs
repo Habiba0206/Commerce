90935e4e1d0faaf32e3a604552505040ad1aa2af
Habiba Sattorova <habibasattorova26@gmail.com>
2025-07-09 03:38:49 +0500

    created sale mapper

---
+++ src/Commerce.Backend/Commerce.Infrastructure/Sales/Mappers/SaleMapper.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Commerce.Application.Sales.Models;
+using Commerce.Domain.Entities;
+
+namespace Commerce.Infrastructure.Sales.Mappers;
+
+public class SaleMapper : Profile
+{
+    public SaleMapper()
+    {
+        CreateMap<Sale, SaleCreateUpdateDto>().ReverseMap();
+        CreateMap<Sale, SaleGetDto>().ReverseMap();
+        CreateMap<Sale, SalePatchDto>().ReverseMap();
+    }
+}
