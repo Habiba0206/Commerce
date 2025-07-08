233ef11408a9ee9019e13ed93320e099a7f244ea
Habiba Sattorova <habibasattorova26@gmail.com>
2025-07-09 03:33:46 +0500

    created get query handler

---
+++ src/Commerce.Backend/Commerce.Infrastructure/Common/Validators/GetQueryValidator.cs
@@ -0,0 +1,18 @@
+﻿using Commerce.Domain.Common.Queries;
+using FluentValidation;
+
+namespace Commerce.Infrastructure.Common.Validators;
+
+public class GetQueryValidator : AbstractValidator<FilterPagination>
+{
+    public GetQueryValidator()
+    {
+        RuleFor(x => x.PageToken)
+            .GreaterThan(0).WithMessage("Token must be greater than 0.")
+            .When(x => x.PageToken.HasValue);
+
+        RuleFor(x => x.PageSize)
+            .InclusiveBetween(1, 100).WithMessage("Size must be between 1 and 100.")
+            .When(x => x.PageSize.HasValue);
+    }
+}
