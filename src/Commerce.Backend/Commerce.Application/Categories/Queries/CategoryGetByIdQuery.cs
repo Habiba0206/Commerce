85b5774141fa4e11351d9941162308d09f431bd5
Habiba Sattorova <habibasattorova26@gmail.com>
2025-07-09 03:15:36 +0500

    created category queries

---
+++ src/Commerce.Backend/Commerce.Application/Categories/Queries/CategoryGetByIdQuery.cs
@@ -0,0 +1,9 @@
+﻿using Commerce.Application.Categories.Models;
+using Commerce.Domain.Common.Queries;
+
+namespace Commerce.Application.Categories.Queries;
+
+public record CategoryGetByIdQuery : IQuery<CategoryGetDto?>
+{
+    public Guid CategoryId { get; set; }
+}
---
+++ src/Commerce.Backend/Commerce.Application/Categories/Queries/CategoryGetQuery.cs
@@ -0,0 +1,9 @@
+﻿using Commerce.Application.Categories.Models;
+using Commerce.Domain.Common.Queries;
+
+namespace Commerce.Application.Categories.Queries;
+
+public record CategoryGetQuery : IQuery<ICollection<CategoryGetDto>>
+{
+    public CategoryFilter CategoryFilter { get; set; }
+}
