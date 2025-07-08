5b175778de3d7ebfbe2834cbe0f30ae81220a137
Habiba Sattorova <habibasattorova26@gmail.com>
2025-07-09 03:41:55 +0500

    created linq extensions

---
+++ src/Commerce.Backend/Commerce.Persistence/Extensions/LinqExtensions.cs
@@ -0,0 +1,42 @@
+﻿using Commerce.Domain.Common.Queries;
+
+namespace Commerce.Persistence.Extensions;
+
+/// <summary>
+/// Provides extension methods for applying query specifications to IQueryable and IEnumerable collections.
+/// </summary>
+public static class LinqExtensions
+{
+    /// <summary>
+    /// Applies pagination to and IQeryable data source.
+    /// </summary>
+    /// <typeparam name="TSource">The type of elements in the data source.</typeparam>
+    /// <param name="sources">The IQueyable source to paginate.</param>
+    /// <param name="filterPagination">Pagination options such as page token and page size.</param>
+    /// <returns>An IQueryable representing the paginated subset of the oeiginal data source.</returns>
+    public static IQueryable<TSource> ApplyPagination<TSource>(
+        this IQueryable<TSource> sources,
+        FilterPagination? filterPagination)
+    {
+
+        return filterPagination == null ? sources : sources
+            .Skip((int)((filterPagination.PageToken - 1) * filterPagination.PageSize))
+            .Take((int)filterPagination.PageSize);
+    }
+
+    /// <summary>
+    /// Applies pagination to and IEnumerable data source.
+    /// </summary>
+    /// <typeparam name="TSource">The type of elements in the data source.</typeparam>
+    /// <param name="sources">The IEnumerable source to paginate.</param>
+    /// <param name="filterPagination">Pagination options such as page token and page size.</param>
+    /// <returns>An IEnumerable representing the paginated subset of the oeiginal data source.</returns>
+    public static IEnumerable<TSource> ApplyPagination<TSource>(
+    this IEnumerable<TSource> sources,
+    FilterPagination? filterPagination)
+    {
+        return filterPagination == null ? sources : sources
+            .Skip((filterPagination.PageToken!.Value - 1) * filterPagination.PageSize!.Value)
+            .Take(filterPagination.PageSize!.Value);
+    }
+}
