994ce47c40412a25371218121016514fde89c765
Habiba Sattorova <habibasattorova26@gmail.com>
2025-07-09 03:41:13 +0500

    created db context

---
+++ src/Commerce.Backend/Commerce.Persistence/DataContexts/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Commerce.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Commerce.Persistence.DataContexts;
+
+public class AppDbContext(DbContextOptions<AppDbContext> dbContextOptions) : DbContext(dbContextOptions)
+{
+    public DbSet<Category> Categories => Set<Category>();
+    public DbSet<Country> Countries => Set<Country>();
+    public DbSet<Product> Products => Set<Product>();
+    public DbSet<ProductManufacturer> ProductManufacturers => Set<ProductManufacturer>();
+    public DbSet<Sale> Sales => Set<Sale>();
+    public DbSet<Section> Sections => Set<Section>();
+}
