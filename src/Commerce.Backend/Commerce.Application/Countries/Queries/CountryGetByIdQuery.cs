8b63a560da2ee1f706d1e5ad09b95bf4d6e94b0b
Habiba Sattorova <habibasattorova26@gmail.com>
2025-07-09 03:18:55 +0500

    created country queries

---
+++ src/Commerce.Backend/Commerce.Application/Countries/Queries/CountryGetByIdQuery.cs
@@ -0,0 +1,9 @@
+﻿using Commerce.Application.Countries.Models;
+using Commerce.Domain.Common.Queries;
+
+namespace Commerce.Application.Countries.Queries;
+
+public record CountryGetByIdQuery : IQuery<CountryGetDto?>
+{
+    public Guid CountryId { get; set; }
+}
---
+++ src/Commerce.Backend/Commerce.Application/Countries/Queries/CountryGetQuery.cs
@@ -0,0 +1,10 @@
+﻿using Commerce.Application.Countries.Models;
+using Commerce.Domain.Common.Queries;
+
+namespace Commerce.Application.Countries.Queries;
+
+public record CountryGetQuery : IQuery<ICollection<CountryGetDto>>
+{
+    public CountryFilter CountryFilter { get; set; }
+}
+
