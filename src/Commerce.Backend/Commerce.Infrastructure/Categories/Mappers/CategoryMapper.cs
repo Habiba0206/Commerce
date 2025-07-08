3573766b18817f34ec7ef50b565fce1e980050a4
Habiba Sattorova <habibasattorova26@gmail.com>
2025-07-09 03:31:27 +0500

    created category mapper

---
+++ src/Commerce.Backend/Commerce.Infrastructure/Categories/Mappers/CategoryMapper.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Commerce.Application.Categories.Models;
+using Commerce.Domain.Entities;
+
+namespace Commerce.Infrastructure.Categories.Mappers;
+
+public class CategoryMapper : Profile
+{
+    public CategoryMapper()
+    {
+        CreateMap<Category, CategoryCreateUpdateDto>().ReverseMap();
+        CreateMap<Category, CategoryGetDto>().ReverseMap();
+        CreateMap<Category, CategoryPatchDto>().ReverseMap();
+    }
+}
