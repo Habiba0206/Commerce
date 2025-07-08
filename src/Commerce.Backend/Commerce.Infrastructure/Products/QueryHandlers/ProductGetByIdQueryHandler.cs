914397805b98db2713c03b7710dc75f62f2c4050
Habiba Sattorova <habibasattorova26@gmail.com>
2025-07-09 03:37:56 +0500

    created product query handlers

---
+++ src/Commerce.Backend/Commerce.Infrastructure/Products/QueryHandlers/ProductGetByIdQueryHandler.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Commerce.Application.Products.Models;
+using Commerce.Application.Products.Queries;
+using Commerce.Application.Products.Services;
+using Commerce.Domain.Common.Queries;
+
+namespace Commerce.Infrastructure.Products.QueryHandlers;
+
+public class ProductGetByIdQueryHandler(
+    IMapper mapper,
+    IProductService service)
+    : IQueryHandler<ProductGetByIdQuery, ProductGetDto>
+{
+    public async Task<ProductGetDto> Handle(ProductGetByIdQuery request, CancellationToken cancellationToken)
+    {
+        var result = await service.GetByIdAsync(request.ProductId, cancellationToken: cancellationToken);
+        return mapper.Map<ProductGetDto>(result);
+    }
+}
---
+++ src/Commerce.Backend/Commerce.Infrastructure/Products/QueryHandlers/ProductGetQueryHandler.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using Commerce.Application.Products.Models;
+using Commerce.Application.Products.Queries;
+using Commerce.Application.Products.Services;
+using Commerce.Domain.Common.Queries;
+using Commerce.Infrastructure.Common.Validators;
+using FluentValidation;
+using Microsoft.EntityFrameworkCore;
+
+namespace Commerce.Infrastructure.Products.QueryHandlers;
+
+public class ProductGetQueryHandler(
+    IMapper mapper,
+    IProductService service,
+    GetQueryValidator validationRules)
+    : IQueryHandler<ProductGetQuery, ICollection<ProductGetDto>>
+{
+    public async Task<ICollection<ProductGetDto>> Handle(ProductGetQuery request, CancellationToken cancellationToken)
+    {
+        var pagination = request.ProductFilter ?? new ProductFilter();
+        var validationResult = await validationRules.ValidateAsync(pagination);
+
+        if (!validationResult.IsValid)
+            throw new ValidationException(validationResult.Errors);
+
+        var result = await service.Get(
+            request.ProductFilter,
+            new QueryOptions { QueryTrackingMode = QueryTrackingMode.AsNoTracking })
+            .ToListAsync(cancellationToken);
+
+        return mapper.Map<ICollection<ProductGetDto>>(result);
+    }
+}
